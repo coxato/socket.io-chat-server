1ea3299923a30001413ebe92d2157f4fc7d13f66
Carlos Martinez <edu6jas@gmail.com>
2020-09-15 19:47:09 -0400

    .

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // routes and middlewares
-app.use(chatRoutes);
+app.use(chatRoutes); 
 
 const server = app.listen(PORT, () => {
     console.log(`server running on localhost:${PORT}`);
