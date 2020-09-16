1d2029e24b5c06d92cf12360781e6368e2ea16de
Carlos Martinez <edu6jas@gmail.com>
2020-09-15 23:55:47 -0400

    intent

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const socketio = require("socket.io");
+const cors = require("cors");
 // routes 
 const chatRoutes = require("./routes/chatRoutes");
 // users
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./services/users');
+const { addUser, removeUser, getUser } = require('./services/users');
 
 // init app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // routes and middlewares
+app.use(cors());
 app.use(chatRoutes); 
 
 const server = app.listen(PORT, () => {
