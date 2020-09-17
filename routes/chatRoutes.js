2ebcf0b5413ce4acb39416a728d98024380cf569
Carlos Martinez <edu6jas@gmail.com>
2020-09-16 22:19:13 -0400

    routes changes

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('server is working')
+    res.send('server is working fine')
 });
 
 module.exports = router;
