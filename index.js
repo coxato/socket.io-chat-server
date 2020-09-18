1ba4a03cfb725dad215f7051fd43a7ff39726c49
Carlos Martinez <edu6jas@gmail.com>
2020-09-18 18:02:30 -0400

    adding routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const socketio = require("socket.io");
 const cors = require("cors");
@@ -12,7 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 // routes and middlewares
 app.use(cors());
-app.use(chatRoutes); 
+app.use(chatRoutes);
+app.use(express.static(path.join(__dirname, 'static')));
 
 const server = app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const path = require("path");
 
 router.get('/', (req, res) => {
     res.send('server is working fine')
+});
+
+router.get('/view', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'static', 'index.html'));
+});
+
+router.get('/json', (req, res) => {
+    res.json({
+        data: 123
+    })
 });
 
 module.exports = router;
